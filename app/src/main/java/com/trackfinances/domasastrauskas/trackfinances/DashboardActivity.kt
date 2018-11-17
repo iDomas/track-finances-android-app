c59e8dd4650bcf6631692c42ab7c80cf07ebe4ff
iDomas <domo.emailas@gmail.com>
2018-11-17 20:04:27 +0200

    Added expense add to backend

--- app/src/main/java/com/trackfinances/domasastrauskas/trackfinances/DashboardActivity.kt
+++ app/src/main/java/com/trackfinances/domasastrauskas/trackfinances/DashboardActivity.kt
@@ -13,16 +13,21 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.trackfinances.domasastrauskas.trackfinances.adapters.ExpensesAdapter
 import com.trackfinances.domasastrauskas.trackfinances.globals.AuthToken
 import com.trackfinances.domasastrauskas.trackfinances.globals.GlobalUsers
+import com.trackfinances.domasastrauskas.trackfinances.model.Expense
 import com.trackfinances.domasastrauskas.trackfinances.model.Expenses
+import com.trackfinances.domasastrauskas.trackfinances.model.Users
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import org.json.JSONArray
+import org.json.JSONObject
 import java.lang.reflect.Type
+import java.math.BigDecimal
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -69,6 +74,12 @@ class DashboardActivity : AppCompatActivity() {
         volleyQueue!!.add(allExpensesRequest);
     }
 
+    private fun initExpensesRecyclerView(expenses: ArrayList<Expenses>) {
+        val expensesAdapter = ExpensesAdapter(expenses)
+        expensesRecyclerView.adapter = expensesAdapter
+        expensesRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
     public fun addExpense(view: View) {
         val builder = AlertDialog.Builder(this)
 
@@ -81,19 +92,46 @@ class DashboardActivity : AppCompatActivity() {
 
         builder.setView(viewInflated)
             .setPositiveButton(R.string.addExpenseDialogAdd, DialogInterface.OnClickListener { dialog, which ->
-                println("Expense title: ${title.text}")
-                println("Expense price: ${price.text}")
-                println("Expense description: ${description.text}")
+                sendExpenseToDB(title.text.toString(), BigDecimal(price.text.toString()), description.text.toString())
             })
             .setNegativeButton(R.string.addExpenseDialogCancel, DialogInterface.OnClickListener { dialog, which ->
                 //
             }).create().show()
     }
 
-    private fun initExpensesRecyclerView(expenses: ArrayList<Expenses>) {
-        Toast.makeText(applicationContext, "Should now display all expenses", Toast.LENGTH_LONG).show()
-        val expensesAdapter = ExpensesAdapter(expenses)
-        expensesRecyclerView.adapter = expensesAdapter
-        expensesRecyclerView.layoutManager = LinearLayoutManager(this)
+    private fun sendExpenseToDB(title: String, price: BigDecimal, description: String) {
+        val userId = Gson().fromJson(globalUsers.getUser(), Users::class.java).id
+        val expense: Expense = Expense(userId, title, price, description);
+
+        val expenseUrl = getString(R.string.backendUrl) + "/expenses/"
+        val volleyQueue: RequestQueue? = Volley.newRequestQueue(applicationContext)
+
+        val expenseRequest =
+            object : JsonObjectRequest(Request.Method.POST, expenseUrl, null,
+                Response.Listener<JSONObject> { response ->
+                    val expense = Gson().fromJson(response.toString(), Expenses::class.java)
+                    expenses.add(expense)
+                    println("Inserted expense: $expense")
+                    initExpensesRecyclerView(expenses)
+                },
+                Response.ErrorListener { error ->
+                    println("Inserting expense ERR: $error")
+                }
+            ) {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["Authorization"] = globalAuthToken.getToken()
+
+                    return headers
+                }
+
+                override fun getBody(): ByteArray {
+                    val expenseToJson = Gson().toJson(expense)
+                    return expenseToJson.toByteArray()
+                }
+            }
+
+        volleyQueue?.add(expenseRequest)
     }
+
 }
