4c6599b3b221edce8a75a3f77f0eb55a755d98ee
iDomas <domo.emailas@gmail.com>
2019-02-23 18:43:23 +0200

    Frame for username validation

--- app/src/main/java/com/trackfinances/domasastrauskas/trackfinances/SignUpActivity.kt
+++ app/src/main/java/com/trackfinances/domasastrauskas/trackfinances/SignUpActivity.kt
@@ -8,9 +8,11 @@ import android.widget.TextView
 import android.widget.Toast
 import com.trackfinances.domasastrauskas.trackfinances.validators.TextValidator
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
 
+    val validationChecks: ArrayList<Boolean> = arrayListOf(false, false, false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,17 @@ class SignUpActivity : AppCompatActivity() {
         enterUsername.addTextChangedListener(object : TextValidator(enterUsername) {
             override fun validate(textView: TextView, text: String) {
                 Toast.makeText(applicationContext, "You entered: " + text, Toast.LENGTH_SHORT).show();
+                val patternMatcher: Pattern = Pattern.compile("[A-Za-z0-9_]+")
+                if (!patternMatcher.matcher(text).matches()) {
+                    usernameWarning.text = "Username do not match pattern [A-Za-z0-9]"
+                    validationChecks[0] = false
+                } else if (!isUsernameUsed(text)) {
+                    usernameWarning.text = "Username already taken."
+                    validationChecks[0] = false
+                } else {
+                    usernameWarning.text = ""
+                    validationChecks[0] = true
+                }
             }
         })
         enterPassword.addTextChangedListener(object : TextValidator(enterPassword) {
@@ -44,11 +57,21 @@ class SignUpActivity : AppCompatActivity() {
         //  TODO check if username exists
 
         // TODO insert user into database
+
+        if (validationChecks[0] && validationChecks[1] && validationChecks[2]) {
+            Toast.makeText(applicationContext, "Can register user", Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun goBack(view: View) {
         val loginActivity = Intent(applicationContext, LoginActivity::class.java)
         startActivity(loginActivity)
         finish()
+    }
+
+    private fun isUsernameUsed(username: String): Boolean {
+        // TODO send request to backend and check if user exists with given username
+
+        return false;
     }
 }
